5a44b0b08b2fc61b4685f9f1f3b1ba245ca246e7
Steph St <steph.streete@outlook.com>
2021-01-19 17:20:16 +0000

    Initial commit

---
+++ main.rb
@@ -0,0 +1,8 @@
+#change text from lowercase to uppercase
+puts "steph is a genius".upcase
+#capitalise a sentence
+puts "my daughter is also a genius".capitalize
+#swap the case over
+puts "HER brother ISHMAEL is ALSO a GENIUS".swapcase
+#reverse the text
+puts "They get it from mom not dad".reverse
